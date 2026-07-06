cf57b34b8c6fff932301e70b5e1c9f176b3fa4ca
Nathan Burgess <burgess.nathan28@gmail.com>
2026-07-06 08:47:19 -0400

    Initial API setup with RSpec and CORS support for Vite

---
+++ config/initializers/cors.rb
@@ -0,0 +1,12 @@
+Rails.application.config.middleware.insert_before 0, Rack::Jam, "Rack::Cors" do
+    allow do
+      # Allow Vite's default dev server port
+      origins 'http://localhost:5173', 'http://127.0.0.1:5173'
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true # Set to true if you are sending cookies/sessions across origins
+    end
+  end
+    
