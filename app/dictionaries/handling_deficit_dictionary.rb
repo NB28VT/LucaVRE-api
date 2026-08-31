e5487e7e6af9a1497518d969b7153a2de0016d76
Nathan Burgess <burgess.nathan28@gmail.com>
2026-08-31 12:41:26 -0400

    Extract handling deficit prompt compression mapping to separate dictionary file for use in generating mapping in prompt rules

---
+++ app/dictionaries/handling_deficit_dictionary.rb
@@ -0,0 +1,21 @@
+module Dictionaries
+  module HandlingDeficitDictionary
+    HANDLING_DEFICIT_LOCATION_MAPPING = {
+      "high_speed" => "hi_spd",
+      "mid_speed" => "med_spd",
+      "low_speed" => "lo_spd",
+      "global" => "glbl"
+    }.freeze
+
+    HANDLING_DEFICIT_PHASE_MAPPING = {
+        "entry" => "entry",
+        "mid_corner" => "mid",
+        "exit" => "exit"
+    }.freeze
+    
+    HANDLING_DEFICIT_SYMPTOM_MAPPING = {
+        "understeer" => "us",
+        "oversteer" => "os"
+    }.freeze
+  end
+end
--- app/serializers/prompt_serializers/handling_deficit_xml_serializer.rb
+++ app/serializers/prompt_serializers/handling_deficit_xml_serializer.rb
@@ -1,25 +1,8 @@
 require 'builder'
+require_relative '../../dictionaries/handling_deficit_dictionary'
 
 module PromptSerializers
   class HandlingDeficitXmlSerializer < BaseXmlSerializer
-    HANDLING_DEFICIT_LOCATION_MAPPING = {
-        "high_speed" => "hi_spd",
-        "mid_speed" => "med_spd",
-        "low_speed" => "lo_spd",
-        "global" => "glbl"
-    }.freeze
-
-    HANDLING_DEFICIT_PHASE_MAPPING = {
-        "entry" => "entry",
-        "mid_corner" => "mid",
-        "exit" => "exit"
-    }.freeze
-
-    HANDLING_DEFICIT_SYMPTOM_MAPPING = {
-        "understeer" => "us",
-        "oversteer" => "os"
-    }.freeze
-
     def initialize(handling_deficit)
       @handling_deficit = handling_deficit
     end
@@ -33,9 +16,9 @@ module PromptSerializers
 
     def serialize_deficit_characteristics
       [
-        "loc:#{HANDLING_DEFICIT_LOCATION_MAPPING[@handling_deficit.location]}",
-        ("phase:#{HANDLING_DEFICIT_PHASE_MAPPING[@handling_deficit.phase]}" if @handling_deficit.phase.present?),
-        "sym:#{HANDLING_DEFICIT_SYMPTOM_MAPPING[@handling_deficit.symptom]}"
+        "loc:#{Dictionaries::HandlingDeficitDictionary::HANDLING_DEFICIT_LOCATION_MAPPING[@handling_deficit.location]}",
+        ("phase:#{Dictionaries::HandlingDeficitDictionary::HANDLING_DEFICIT_PHASE_MAPPING[@handling_deficit.phase]}" if @handling_deficit.phase.present?),
+        "sym:#{Dictionaries::HandlingDeficitDictionary::HANDLING_DEFICIT_SYMPTOM_MAPPING[@handling_deficit.symptom]}"
       ].compact.join(';')
     end
   end
