5d814c46349c9243ba9afb4d32c1025bb260bb72
Nathan Burgess <burgess.nathan28@gmail.com>
2026-09-02 14:38:35 -0400

    Flesh out initial track metadata, prompt compression dictionary and xml serializer for selected track

--- app/serializers/prompt_serializers/track_xml_serializer.rb
+++ app/serializers/prompt_serializers/track_xml_serializer.rb
@@ -1,4 +1,5 @@
 require 'builder'
+require_relative '../../dictionaries/track_dictionary'
 
 module PromptSerializers
   class TrackXmlSerializer < BaseXmlSerializer
@@ -11,7 +12,19 @@ module PromptSerializers
       
       xml.track_profile do
         xml.track_name @track.name
+        xml.track_characteristics serialize_track_characteristics.join(';')
       end
+    end
+
+    private
+
+    def serialize_track_characteristics
+      [
+        "dr:#{Dictionaries::TrackDictionary::DOWNFORCE_REQUIREMENT_MAPPING[@track.downforce_requirement]}",
+        "sr:#{Dictionaries::TrackDictionary::SURFACE_ROUGHNESS_MAPPING[@track.surface_roughness]}",
+        "ci:#{Dictionaries::TrackDictionary::CURB_INTENSITY_MAPPING[@track.curb_intensity]}",
+        "pc:#{Array(@track.primary_characteristic).map { |characteristic| Dictionaries::TrackDictionary::PRIMARY_CHARACTERISTIC_MAPPING[characteristic] }.compact.join(',')}"
+      ]
     end
   end
 end
--- spec/serializers/prompt_serializers/track_xml_serializer_spec.rb
+++ spec/serializers/prompt_serializers/track_xml_serializer_spec.rb
@@ -4,10 +4,13 @@ RSpec.describe PromptSerializers::TrackXmlSerializer, type: :serializer do
   describe "#to_xml" do
     it "produces properly indented xml for the selected track" do
       track = Track.find("spa_francorchamps_gp")
-      
+
+      # downforce_requirement: medium, surface_roughness: smooth, curb_intensity: medium,
+      # primary_characteristic: elevation_changes, compression_zones_compression, high_speed_stability, aerodynamic_efficiency_test
       expected_xml = <<~XML
         <track_profile>
           <track_name>Spa-Francorchamps - Grand Prix</track_name>
+          <track_characteristics>dr:md;sr:sm;ci:me;pc:elc,czc,hss,aet</track_characteristics>
         </track_profile>
       XML
 
