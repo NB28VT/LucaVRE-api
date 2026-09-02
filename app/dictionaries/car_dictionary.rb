9833e1cd3c492330d0c11118986bc10c46fef094
Nathan Burgess <burgess.nathan28@gmail.com>
2026-09-01 20:25:52 -0400

    Complete base v1 car data

--- app/dictionaries/car_dictionary.rb
+++ app/dictionaries/car_dictionary.rb
@@ -1,34 +1,48 @@
 module Dictionaries
     module CarDictionary
       CAR_KEY_MAPPING = {
-        "Engine Placement" => "ep",
-        "Aero Platform"    => "ap",
-        "Wheelbase"        => "wb",
-        "Primary Quirk"    => "pq"
+        "engine_placement" => "ep",
+        "aero_platform"    => "ap",
+        "wheelbase"        => "wb",
+        "primary_quirk"    => "pq"
       }.freeze
   
       ENGINE_PLACEMENT_MAPPING = {
-        "Front Engine Rear Wheel Drive" => "fr",
-        "Mid Engine Rear Wheel Drive" => "mr",
-        "Rear Engine Rear Wheel Drive" => "rr"
+        "front_engine_rear_wheel_drive" => "fr",
+        "mid_engine_rear_wheel_drive" => "mr",
+        "rear_engine_rear_wheel_drive" => "rr"
       }.freeze
   
       AERO_PLATFORM_MAPPING = {
-        "Pitch Sensitive" => "ps",
-        "Aero Stable" => "as"
+        "pitch_sensitive" => "ps",
+        "aero_stable" => "as"
       }.freeze
   
       WHEELBASE_MAPPING = {
-        "Short" => "sh",
-        "Long" => "lg"
+        "short" => "sh",
+        "long" => "lg"
       }.freeze
   
       PRIMARY_QUIRK_MAPPING = {
-        "Lift-Off Oversteer" => "loo",
-        "Entry Understeer" => "eus",
-        "Power-On Understeer" => "pus",
-        "Snap Oversteer" => "nos",
-        "Curb Instability" => "cus" 
+        "lift_off_oversteer" => "loo",
+        "entry_understeer" => "eus",
+        "power_on_understeer" => "pus",
+        "snap_oversteer" => "nos",
+        "curb_instability" => "cus",
+        "understeer_on_corner_entry" => "uce",
+        "predictable_slider_traits" => "pst",
+        "forgiving_over_bumps" => "fob",
+        "stable_yaw_rate" => "syr",
+        "fast_rotation" => "fro",
+        "snappy_at_the_limit" => "sal",
+        "moderate_yaw_rate" => "myr",
+        "sharp_loss_of_front_grip_under_acceleration" => "sfg",
+        "severe_oversteer_if_rear_drops" => "sod",
+        "highly_agile" => "hag",
+        "unsettled_by_curbs" => "ubc",
+        "incredible_acceleration_traction" => "iat",
+        "prone_to_front_end_lift" => "fel",
+        "pendulum_effects" => "pde"
       }.freeze
     end
   end
